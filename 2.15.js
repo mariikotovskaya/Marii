8267eb1b1d55acdc3160021aec1b586fddac5156
Maria Karelskaya <m.kotovskaya@tandp.ru>
2024-05-19 09:35:11 +0300

    com for

---
+++ 2.15.js
@@ -0,0 +1,17 @@
+// 1 отличий нет
+// 2 задание Разобраться что именно выполняет директива return
+
+  function checkAge(age)
+ return (age > 18) ? true :  confirm('Родители разрешили?');
+
+ function checkAge(age)
+ return (age > 18) || true :  confirm('Родители разрешили?');
+
+ // 3 задание
+ function min(a, b) 
+    if (a < b) 
+      return a;
+     else 
+      return b
+
+
